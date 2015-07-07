adf3762cc7ce8756907fa816d870e806804514a7
Chris Dawes <cd250693@gmail.com>
2015-07-07 21:24:00 +0800

    inital commit after changing to sublime for editing, class is starting to take shape

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#     -*- coding: utf-8 -*-
 import subprocess
 import commands
 import time
--- baxter-rubiks.py
+++ baxter-rubiks.py
@@ -7,6 +7,10 @@ import numpy as np
 import requests
 import argparse
 from matplotlib import pyplot as plt
+import logging
+
+logger = logging.getLogger(__name__)
+logging.getLogger('requests').setLevel(logging.WARNING)
 
 
 class BaxterRubiks(object):
@@ -15,35 +19,48 @@ class BaxterRubiks(object):
     and Cube Explorer
     """
 
-    def __init__(self):
+    def __init__(self, loglevel):
         """
-        initializes object"""
-        pass
+        initializes object
+        """
+        self.loglevel = loglevel
 
-    def run_solver(program):
+    def run_solver(self, program):
         """
         runs program
         """
         pass
 
-    def exit_solver(program):
+    def exit_solver(self, program):
         """
         exit program
         """
         pass
 
-    def check_solver_status():
+    def check_solver_status(self):
         """
         checks whether the solver is running
         """
         pass
 
 
+def parse_arguments():
+    """
+    sets up argument parsing
+    """
+    parser = argparse.ArgumentParser(prog='baxter-rubiks-algorithm',
+                                     description='Solves a rubiks cube using the baxter research robot and a rubiks solver',
+                                     formatter_class='argparse.RawTextHelpFormatter')
+    parser.add_argument('-v', '--verbose', action='store_true', help='increases verbosity')
+    return parser.parse_args()
+
+
 def main():
     """
-    Creates a BaxterRubiks object and runs the control function
+    Gets command line arguments, creates a BaxterRubiks object and runs the control function
     """
-    cube_solve = BaxterRubiks()
+    args = parse_arguments()
+    cube_solve = BaxterRubiks(args.verbose)
     cube_solve  # need to connect this to control function
 
 if __name__ == '__main__':
