b04ba05b1235dbb8aba4550d5aa98573d9c4f2b6
Chris Dawes <cd250693@gmail.com>
2015-07-07 22:58:20 +0800

    wrote logger and solver functions within class

--- baxter-rubiks.py
+++ baxter-rubiks.py
@@ -25,23 +25,100 @@ class BaxterRubiks(object):
         """
         self.loglevel = loglevel
 
-    def run_solver(self, program):
+    def setup_logger(self, loggerlevel):
         """
-        runs program
+        Sets up the logging with the specified level
         """
-        pass
-
-    def exit_solver(self, program):
-        """
-        exit program
-        """
-        pass
+        logformat = '%(asctime)s: %(message)s'
+        if loggerlevel:
+            logging.basicConfig(level=logging.DEBUG, format=logformat)
+        else:
+            logging.basicConfig(level=logging.INFO, format=logformat)
 
     def check_solver_status(self):
         """
         checks whether the solver is running
         """
-        pass
+        processes = commands.getoutput('ps-A')
+        if 'cube512htm' in processes:
+            return True
+        else:
+            return False
+
+    def run_solver(self):
+        """
+        runs Cube Explorer if it is not already running
+        """
+        if self.check_solver_status() is False:
+            solver = subprocess.Popen('solver/cube512htm.exe')
+            logger.info('Cube Explorer started')
+            return solver
+        else:
+            logger.info('Cube Explorer already running')
+            return True
+
+    def exit_solver(self, program):
+        """
+        exits Cube Explorer
+        """
+        if program is True:
+            logger.info('Cube Explorer was initially open, leaving open')
+        else:
+            close_cube_explorer = raw_input('Close Cube Explorer? (Y/N)')
+            if close_cube_explorer == 'y':
+                logger.info('Now exiting')
+                subprocess.Popen.kill(program)
+                subprocess.Popen.wait(program)
+            elif close_cube_explorer == 'n':
+                logger.info('Leaving open')
+            else:
+                logger.info('Invalid input, now exiting')
+
+    def check_solver_connection(self):
+        """
+        Checks the connection to Cube Explorers webserver
+        """
+        logger.info('Checking connection to Cube Explorer')
+        # Define loop variable to allow a timeout
+        loop = 12
+        while loop > 0:
+            try:
+                # send status request to the webserver
+                requests.get('http://127.0.0.1:8081/?status')
+                logger.info('Connection successful')
+                return True
+            except requests.exceptions.ConnectionError:
+                time.sleep(2.5)
+                loop += 1
+        else:
+            logger.error('Couldn\'t connect to webserver')
+            return False
+
+    def send_solver_face_encoding(self, face_encoding):
+        """
+        Sends the face encoding to the solver, must already be in singmaster
+        notation. Returns the manouvers as a list with html encoding removed.
+        """
+        # get the status of the connection to the webserver
+        solver_connection_status = self.check_solver_connection()
+        if solver_connection_status is True:
+            try:
+                response = requests.get('http://127.0.0.1:8081/?' + face_encoding)
+                # clear the main window
+                requests.get('http://127.0.0.1:8081/?clear')
+            except requests.exceptions.ConnectionError:
+                return False
+            manouvers_raw = response.text
+            # removes the html encoding from the response
+            manouvers = manouvers_raw.replace('<HTML><BODY>\r\n', '')
+            manouvers = manouvers.replace('\r\n</BODY></HTML>\r\n', '')
+            # check to see if the cube was solved
+            if 'Cube cannot be solved' in manouvers:
+                logger.error('{}'.format(manouvers))
+                return False
+            else:
+                # returns manouvers as a list
+                return manouvers.split()
 
 
 def parse_arguments():
