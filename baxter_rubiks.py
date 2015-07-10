c41cf3c94a8724dc6e8d01be00c148ffad5fa5a1
Chris Dawes <cd250693@gmail.com>
2015-07-10 23:01:13 +0800

    renamed

--- baxter_rubiks.py
+++ baxter_rubiks.py
@@ -120,13 +120,59 @@ class BaxterRubiks(object):
                 # returns manouvers as a list
                 return manouvers.split()
 
+    def convert_to_singmaster(self, cube_colour_encoding):  # needs changing when the cube_colour_encoding is ordered properly
+        """
+        Convers face colour encoding to singmaster notation
+        Outputs as a string in the order required for Cube Explorer:
+        up - right - front - down - left - back
+        """
+        # reorders cube_colour_encoding ----------------------- maybe save in propper order first time around!?!?!?
+        # this asumes the ordering is front - back - left - right - up - down
+        # just remove the following statement when cube_colour_encoding is ordered properly.....
+        required_order = [2, 5, 0, 3, 4, 1]
+        cube_colour_encoding = [cube_colour_encoding[i] for i in required_order]
+        # get the centre colour of each face
+        front_colour = cube_colour_encoding[0][4]
+        back_colour = cube_colour_encoding[1][4]
+        up_colour = cube_colour_encoding[2][4]
+        down_colour = cube_colour_encoding[3][4]
+        left_colour = cube_colour_encoding[4][4]
+        right_colour = cube_colour_encoding[5][4]
+        # combines face colour strings into one string
+        cube_colour_encoding_joined = ''.join(cube_colour_encoding)
+        # replaces colour letter with its corresponding face letter
+        cube_centre_colours = ['f', 'b', 'u', 'd', 'l', 'r']
+        for colour in cube_colour_encoding_joined:
+            if colour in
+        cube_colour_encoding_singmaster = cube_colour_encoding_joined.replace(front_colour, 'f').replace(back_colour, 'b').replace(up_colour, 'u').replace(down_colour, 'd').replace(left_colour, 'l').replace(right_colour, 'r')
+
+# classes : control and cube explorer, vision system, manipulations, a class to act as variable storage for each face
+# (create an instance for each face required)
+# --------------------------------------------------------------------------------
+# control and cube explorer: main control class, contains setup_logger, check_solver_status, run_solver,
+#                            exit_solver, check_solver_connection, send_solver_face_encoding, convert_to_singmaster
+# --------------------------------------------------------------------------------
+# vision system: get images from camera, general vision system manipulations
+# --------------------------------------------------------------------------------
+# manipulations: pickup cube, scan faces (one for each face?), face manipulations, place cube back down
+# --------------------------------------------------------------------------------
+# face class: contains variables for each face cubie value (on instance per face)
+# --------------------------------------------------------------------------------
+
+
+class VisionSystem(object):
+    """
+    Analyses images of cube faces and returns colour values
+    """
+
 
 def parse_arguments():
     """
     sets up argument parsing
     """
     parser = argparse.ArgumentParser(prog='baxter-rubiks-algorithm',
-                                     description='Solves a rubiks cube using the baxter research robot and a rubiks solver',
+                                     description=('Solves a rubiks cube using the baxter research ' +
+                                                  'robot and a rubiks solver'),
                                      formatter_class='argparse.RawTextHelpFormatter')
     parser.add_argument('-v', '--verbose', action='store_true', help='increases verbosity')
     return parser.parse_args()
