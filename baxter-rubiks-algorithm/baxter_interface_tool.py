cb8b56e3eba626dfc331b4a33ff345387cf70c2c
Chris Dawes <cd250693@gmail.com>
2015-10-01 12:24:59 +0800

    finished intialization, wrote position recording and playback

--- baxter-rubiks-algorithm/baxter_interface_tool.py
+++ baxter-rubiks-algorithm/baxter_interface_tool.py
@@ -4,9 +4,13 @@ import argparse
 import rospy
 
 import baxter_interface
+from baxter_interface import CHECK_VERSION
+from sensor_msgs.msg import Image
 
 import cv2
 import cv_bridge
+
+logger = logging
 
 
 class BaxterInterfaceTool(object):
@@ -21,29 +25,86 @@ class BaxterInterfaceTool(object):
         """
         self.loglevel = loglevel
 
+        # Initialize the rospy node
+        logger.info('initializing node')
+        rospy.init_node('baxter_interface_tool')
+        # Register the clean shutdown function, which is called before
+        # shutting down
+        rospy.on_shutdown(self.clean_shutdown)
+
         # Create baxter_interface limb instances
-        left_limb = baxter_interface.Limb('left')
-        right_limb = baxter_interface.Limb('right')
-        grip_left = baxter_interface.Gripper('left', CHECK_VERSION)
-        grip_right = baxter_interface.Gripper('right', CHECK_VERSION)
+        self.left_limb = baxter_interface.Limb('left')
+        self.right_limb = baxter_interface.Limb('right')
+        self.grip_left = baxter_interface.Gripper('left', CHECK_VERSION)
+        self.grip_right = baxter_interface.Gripper('right', CHECK_VERSION)
 
         # Create limb positions container
         self.left_positions = list()
         self.right_positions = list()
+        # Create variable to keep track of recording state
+        self.recording_positions = False
 
         # Verify robot is enabled
         logger.info('Getting robot state')
-        self._robotstate = baxter_interface.RobotEnable()
-        self._initial_state = self._robotstate.state().enabled
+        self.robotstate = baxter_interface.RobotEnable()
+        self.initial_state = self.robotstate.state().enabled
         logger.info('Enabling robot')
-        self._robotstate.enable()
+        self.robotstate.enable()
 
         # Create Navitaors I/O
-        self._left_navigator_io = baxter_interface.Navigator('left')
-        self._right_naviagator_io = baxter_interface.Navigator('right')
+        self.left_navigator_io = baxter_interface.Navigator('left')
+        self.right_naviagator_io = baxter_interface.Navigator('right')
 
         # Image path to display on baxters face camera
-        self._img_path = 'rubiks_algorithm_image.jpg'
+        self.img_path = 'rubiks_algorithm_image.jpg'
+
+    def record_positions(self):
+        """
+        function to record the positions of the arms
+        """
+        logger.info('recording positions')
+        # Connect the navigator io signals to the callback functions
+        # on scroll wheel press
+        self.left_navigator_io.button0_changed.connect(self.record_position_left)
+        self.right_naviagator_io.button0_changed.connect(self.record_position_right)
+        # on navigator rething button press (either arm)
+        self.left_navigator_io.button1_changed.connect(self.stop_recording)
+        self.right_naviagator_io.button1_changed.connect(self.stop_recording)
+
+        # Set the recording flag
+        self.recording_positions = True
+
+        # Loop until waypoints are done being recorded
+        while self.recording_positions and not rospy.is_shutdown():
+            rospy.sleep(1.0)
+
+        # Release the navigator I/O signals, disconnecting the callback functions
+        self.left_navigator_io.button0_changed.disconnect(self.record_position_left)
+        self.right_naviagator_io.button0_changed.disconnect(self.record_position_right)
+        self.left_navigator_io.button1_changed.disconnect(self.stop_recording)
+        self.right_naviagator_io.button1_changed.disconnect(self.stop_recording)
+
+    def playback_positions(self):
+        """
+        Plays back the recorded positions once
+        """
+        rospy.sleep(1.0)
+
+        logger.info('Position playback begun')
+
+        while not rospy.is_shutdown():
+            for position in self.left_positions:
+                if rospy.is_shutdown():
+                    break
+                self.left_limb.move_to_joint_positions(position, timeout=20.0)
+
+        rospy.sleep(3.0)
+
+        while not rospy.is_shutdown():
+            for position in self.right_positions:
+                if rospy.is_shutdown():
+                    break
+                self.right_limb.move_to_joint_positions(position, timeout=20.0)
 
     def setup_logger(self, loggerlevel):
         """
@@ -55,18 +116,18 @@ class BaxterInterfaceTool(object):
         else:
             logging.basicConfig(level=logging.INFO, format=logformat)
 
-    def send_image():
+    def send_image(self):
         """
         Send the image specified to the baxter head screen
         """
-        img = cv2.imread(self._img_path)
+        img = cv2.imread(self.img_path)
         msg = cv_bridge.CvBridge().cv2_to_imgmsg(img, encoding='bgr8')
         pub = rospy.Publisher('/robot/xdisplay', Image, latch=True, queue_side=1)
         pub.publish(msg)
         #Sleep to allow the image to be published
         rospy.sleep(1)
 
-    def _record_position_left(self, value):
+    def record_position_left(self, value):
         """
         Stores the current left arm position when the
         Navigator OK button is pressed
@@ -75,7 +136,7 @@ class BaxterInterfaceTool(object):
             logger.info('angles for left arm recorded')
             self.left_positions.append(self.left_limb.joint_angles())
 
-    def _record_position_right(self, value):
+    def record_position_right(self, value):
         """
         Stores the current right arm position when the
         Navigator OK button is pressed
@@ -84,6 +145,20 @@ class BaxterInterfaceTool(object):
             logger.info('angles for right arm recorded')
             self.right_positions.append(self.right_limb.joint_angles())
 
+    def stop_recording(self, value):
+        """
+        Sets recording_positions to false and ends the recording loop
+        """
+        if value:
+            self.recording_positions = False
+
+    def clean_shutdown(self):
+        logger.info('Exiting')
+        if not self.initial_state:
+            logger.info('Disabling Robot')
+            self.robotstate.disable()
+        return True
+
 
 def parse_arguments():
     """
@@ -91,7 +166,7 @@ def parse_arguments():
     """
     parser = argparse.ArgumentParser(prog='baxter-interface-tool',
                                      description='tool using the baxter interface api',
-                                     formatter_class=argeparse.RawTextHelpFormatter)
+                                     formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-v', '--verbose', action='store_true', help='increase verbosity')
     return parser.parse_args()
 
@@ -101,8 +176,11 @@ def main():
     Gets command line arguments, creates a BaxterInterfaceTool object and run control function
     """
     args = parse_arguments()
-    baxter_interface_tool = BaxterInterfaceTool(args.verbose, args.limb)
-    baxter_interface_tool."here needs the control function"()
+    baxter_interface_tool = BaxterInterfaceTool(args.verbose)
+    baxter_interface_tool.send_image()
+    baxter_interface_tool.record_positions()
+    baxter_interface_tool.playback_positions()
+
 
 if __name__ == '__main__':
     main()
